0e7a87c92ef8c73de6877b309c14d14dfccbbe3d
ksr-ccb <harang4282@gmail.com>
2025-03-10 11:39:07 +0900

    [추가] 도전 lv.1 아마 끝?

---
+++ src/challenge/lv1/Kiosk.java
@@ -0,0 +1,162 @@
+package challenge.lv1;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    private List<Menu> menuList;
+    private ShoppingCart shoppingCart;
+    private final Scanner sc = new Scanner(System.in);
+
+
+    private int categoryInput = 0;
+    private int itemInput = 0;
+
+    private boolean exitFlag = true;
+    private MenuItem selectMenu;
+    private boolean isCartEmpty = true;
+
+    Kiosk(List<Menu> menuList, ShoppingCart shoppingCart){
+        this.menuList = menuList;
+        this.shoppingCart = shoppingCart;
+    }
+
+    public void startKiosk() {
+        System.out.println("방문을 환영합니다. 서비스를 시작합니다.");
+        exitFlag =true;
+        boolean cont;
+        boolean showMenu = false;
+
+        while(exitFlag){
+            inputCategory(); //카테고리 보여주고 입력받기
+            Menu selectedMenu = menuList.get(categoryInput-1);
+            
+            cont = inputMenu(selectedMenu);// 입력받은 카테고리에 맞는 메뉴 보여주고 메뉴 입력받기
+            if(!cont){
+                continue;
+            }
+            selectMenu = selectedMenu.selectMenuItem(itemInput - 1);
+            System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+            System.out.println("1. 장바구니에 담기   |   2. 메뉴판으로 ");
+            inputOrder();
+        }
+    }
+
+    private void printCategory(){
+        int length = menuList.size();
+        //카테고리 출력
+        System.out.println("========================================< 맘스땃쥐 >============================================");
+        for (int i = 0; i <length; i++) {
+            System.out.println("|| " + (i + 1) + ". " + menuList.get(i).getCategoryName());
+        }
+        System.out.println("|| 0. 나가기 ");
+        System.out.println("===============================================================================================");
+        isCartEmpty = shoppingCart.orderMenu(length+1);
+    }
+
+    private void inputCategory(){
+        int length=0;
+        boolean flag = true;
+        while (flag) {
+            printCategory();
+            if(isCartEmpty){
+                length = menuList.size();
+            }else{
+                length = menuList.size()+2;
+            }
+
+            try {
+                categoryInput = sc.nextInt();
+                if (categoryInput < 0 || categoryInput > length) {
+                    throw new InputMismatchException();
+                } else if (categoryInput == 0) {
+                    System.out.println("이용해주셔서 감사합니다.");
+                    return;
+                }else{
+                    if( categoryInput <= menuList.size()){
+                        System.out.println(menuList.get(categoryInput-1).getCategoryName()+"(이)가 선택되었습니다.");
+                        flag=false;
+                    }else if (categoryInput == menuList.size() +1){
+                        // 장바구니 출력
+                        System.out.println("주문하기(이)가 선택되었습니다. 장바구니를 출력합니다.");
+                        shoppingCart.printCart();
+                        inputPayment();
+                    }else{
+                        //장바구니 초기화
+                        System.out.println("주문 취소 하기(이)가 선택되었습니다. 장바구니를 초기화합니다.");
+                        shoppingCart.resetCart();
+                    }
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("메뉴에 있는 번호를 입력해주세요!");
+                sc.nextLine();
+            }
+        }
+    }
+
+    private boolean inputMenu(Menu selectedMenu){
+        boolean flag = true;
+        while (flag) {
+            selectedMenu.printMenuItems(); //해당 카테고리의 음식 출력
+            try {
+                itemInput = sc.nextInt();
+                if (itemInput < 0 || itemInput > selectedMenu.getMenuItems().size()) {
+                    throw new InputMismatchException();
+                } else if (itemInput == 0) {
+                    System.out.println("카테고리로 돌아갑니다.");
+                    return false;
+                }
+                return true;
+            } catch (InputMismatchException e) {
+                System.out.println("메뉴에 있는 번호를 입력해주세요!");
+                sc.nextLine();
+            }
+        }
+        return true;
+    }
+
+    private void inputPayment(){
+        while (true) {
+            try {
+                int paymentInput = sc.nextInt();
+                if (paymentInput == 1){
+                    System.out.println("장바구니 목록을 구매합니다.");
+
+                    shoppingCart.resetCart(); //장바구니 초기화
+                    System.out.println("초기화면으로 돌아갑니다.");
+                    break;
+                } else if (paymentInput == 2) {
+                    System.out.println("구매를 진행하지 않고 초기화면으로 돌아갑니다.");
+                    break;
+                } else {
+                    throw new InputMismatchException();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("메뉴에 있는 번호를 입력해주세요!");
+                sc.nextLine();
+            }
+        }
+    }
+    private void inputOrder(){
+        while (true) {
+            try {
+                int orderInput = sc.nextInt();
+                if (orderInput == 1) {
+                    shoppingCart.addCart(selectMenu);
+                    System.out.println(selectMenu.getName() + "를 장바구니에 담습니다.");
+                    System.out.println("초기화면으로 돌아갑니다.");
+                    break;
+                } else if (orderInput == 2) {
+                    System.out.println("구매를 진행하지 않고 초기화면으로 돌아갑니다.");
+                    break;
+                } else {
+                    throw new InputMismatchException();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("메뉴에 있는 번호를 입력해주세요!");
+                sc.nextLine();
+            }
+        }
+    }
+}
