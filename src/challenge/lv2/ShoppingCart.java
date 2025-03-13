ce4a0c7ebc78cb5fecc06d9f7b0f038f75e962da
ksr-ccb <harang4282@gmail.com>
2025-03-13 17:10:10 +0900

    💡 challenge Lv.2 Writing Javadoc content

--- src/challenge/lv2/ShoppingCart.java
+++ src/challenge/lv2/ShoppingCart.java
@@ -3,13 +3,32 @@ package challenge.lv2;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * {@code ShoppingCart} 클래스는 장바구니 객체입니다.
+ * 장바구니가 가질 수 있는 속성들인 {@link MenuItem} 리스트와 각각의 물품 갯수를 저장하는 count 리스트, 가격을 계산하는 price리스트가 있습니다.
+ * 그 외에도 간단하게 get함수로 값을 얻기 위해 totalPrice, totalCount을 global로 사용합니다.
+ */
 public class ShoppingCart {
-    private List<MenuItem> menuList = new ArrayList<MenuItem>(); //장바구니에 담은 메뉴
-    private List<Integer> count = new ArrayList<Integer>(); // 메뉴 몇개씩 샀나
-    private List<Integer> price = new ArrayList<Integer>();
+    /** 장바구니 목록 리스트 */
+    private final List<MenuItem> menuList = new ArrayList<MenuItem>(); //장바구니에 담은 메뉴
+
+    /** 각 메뉴를 몇개씩 샀는지 저장하는 리스트 */
+    private final List<Integer> count = new ArrayList<Integer>(); // 메뉴 몇개씩 샀나
+
+    /** 각 메뉴의 가격을 갯수에 맞게 저장하는 리스트*/
+    private final List<Integer> price = new ArrayList<Integer>();
+
+    /** 최종 계산에서 쓰이는 합계 가격*/
     private int totalPrice =0;
+
+    /** 장바구니 안에 들어간 메뉴의 개수*/
     private int totalCount = 0;
 
+    /**
+     * 장바구니에 메뉴를 추가합니다.
+     * @param item 이 추가됩니다.
+     *             만약 같은 메뉴가 이미 담겨있다면 갯수와 가격 변동만 해줍니다.
+     */
     public void addCart(MenuItem item){
 
         if(menuList.contains(item)){
@@ -26,10 +45,18 @@ public class ShoppingCart {
         totalCount++;
     }
 
+    /**
+     * 장바구니에 들어간 메뉴의 가짓수를 제공합니다.
+     * @return 메뉴리스트의 길이를 리턴합니다.
+     */
     public int getCartLength(){
         return menuList.size();
     }
 
+    /**
+     * 장바구니 목록을 출력합니다.
+     * 목록과 담겨있는 메뉴들의 총 금액도 출력합니다.
+     */
     public void printCart(){ //장바구니 목록 출력
         totalPrice=0;
         System.out.println("========================<장바구니 목록>==========================");
@@ -42,10 +69,20 @@ public class ShoppingCart {
         System.out.println("=============================================================");
     }
 
+    /**
+     * 총 금액을 반환합니다.
+     */
     public int getTotalPrice(){
         return totalPrice;
     }
 
+    /**
+     * 카테고리 화면에서 장바구니 관련 내용을 출력
+     * 만약 장바구니에 담긴 메뉴가 없다면 넘어가고, 있으면 카테고리 개수 +1에는 주문하기, +2에는 주문 취소하기를 출력합니다.
+     * @param num 출력하고 잇는 카테고리의 총 개수
+     * @return {@code true} - 장바구니 선택이 불가능한 경우
+     *         {@code false} - 장바구니 선택이 가능한 경우 (사용자 입력 바운더리를 나중에 조정)
+     */
     public boolean orderMenu(int num){ // 장바구니 메뉴있으면 밑에꺼, 없으면 출력안하게 해줌
         if(menuList.isEmpty()){
             System.out.println("주문하실 카테고리를 선택하세요.");
@@ -60,6 +97,10 @@ public class ShoppingCart {
         }
     }
 
+    /**
+     * 할인 금액을 계산하고 출력.
+     * @param discountType 들어온 타입에 맞게 할인을 진행하고 총 금액을 조정합니다.
+     */
     public void applyDiscount(DiscountType discountType){ // 할인종류 받아서 전체금액 조정
         totalPrice = discountType.discountPrice(totalPrice);
         System.out.println("======================<결제하실 금액>===========================");
@@ -67,6 +108,10 @@ public class ShoppingCart {
         System.out.println("=============================================================");
     }
 
+    /**
+     * 장바구니를 초기화합니다(안의 내용을 모두 비워줍니다.
+     * totalPrice같은 경우 장바구니 목록에 들어가야지 볼 수 있는데, 목록에 들어갈때 새롭게 계산하기 때문에 초기화 필요가 없습니다.
+     */
     public void resetCart(){ //장바구니 초기화
         menuList.clear();
         count.clear();
@@ -74,6 +119,10 @@ public class ShoppingCart {
         totalCount = 0;
     }
 
+    /**
+     * 장바구니 수정 화면을 출력.
+     * 기존의 목록 출력과 다르게 앞에 사용자 입력을 받을 수 있게 번호가 붙습니다.
+     */
     public void printEditMenu(){
         totalPrice=0;
         System.out.println("========================<장바구니 목록>==========================");
@@ -87,6 +136,10 @@ public class ShoppingCart {
         System.out.println("=============================================================");
     }
 
+    /**
+     * 장바구니 수정 메서드
+     * @param index 를 가지는 menuList를 제거해주고 그것에 맞게 수량과 금액을 조정합니다.
+     */
     public void setCartItem(int index){
         System.out.println((index+1) + "번 "+menuList.get(index).getName() + "을 장바구니에서 제외합니다.");
 
--- src/challenge/lv2/UserInputManager.java
+++ src/challenge/lv2/UserInputManager.java
@@ -3,9 +3,23 @@ package challenge.lv2;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+
+/**
+ * {@code UserInputManager} 클래스는 장바구니 객체입니다.
+ * 키오스크에서 필요한 입력값을 바로 해당되는 자료형으로 받기 위해서, 입력값의 형태를 검증합니다.
+ * inputIntegerValue 함수이외에도 다른 입력값이 피필요다하면 inputDoubleValue, inputStringValue 등 여러 함수가 추가될 수 있습니다.
+ */
 public class UserInputManager {
+    /** 사용자 인풋을 받기위한 스캐너 */
     private final Scanner sc = new Scanner(System.in);
 
+    /**
+     * 받아온 입력값이 정수인지 확인하는 함수입니다.
+     * @return {@code Integer.parseInt(inputStr)} - 정수값을 입력한 경우
+     * @throws InputMismatchException - 그 외의 입력값은 모두 오류로 취급하여 throws 합니다.
+     * 오류 처리는 kiosk에서 합니다.
+     * 인풋 값의 바운더리가 상황에 따라서 계속 변동되기 때문에 try-catch문이 Kiosk에서 필요하기 떄문입니다.
+     */
     public int inputIntegerValue() throws InputMismatchException{
             //정수만 입력받아야 함.
         String inputStr = sc.nextLine().trim();
